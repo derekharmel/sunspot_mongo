83563e84f4f55e591c2b5d570a612a575da87b59
Derek Harmel <dgharmel@gmail.com>
2012-03-04 21:18:59 -0500

    Added code to spec_helper for starting up a Solr instance and MongoDB database to test against

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,3 +8,28 @@ Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb' ))].
 
 # Load shared examples
 require "shared_examples"
+
+def setup_servers_and_connections
+  FileUtils.mkdir_p '/tmp/sunspot_mongo_test/'
+
+  @solr_pid  = fork { `sunspot-solr start --log-file=/tmp/sunspot_mongo/solr.log --log-level=INFO --port=8900` }
+  @mongo_pid = fork { `mongod --fork --port 27900 --logpath=/tmp/sunspot_mongo/mongo.log --dbpath=/tmp/sunspot_mongo/` }
+  sleep 5
+
+  connection = Mongo::Connection.new('localhost', 27900)
+  database   = connection.db('sunspot_mongo_test')
+  database.collections.reject{|col| col.name =~ /^system/}.each &:drop
+
+  Mongoid.database       = database
+  MongoMapper.connection = connection
+  MongoMapper.database   = 'sunspot_mongo_test'
+
+  Sunspot.config.solr.url = 'http://127.0.0.1:8900/solr'
+end
+setup_servers_and_connections
+
+RSpec.configure do |config|
+  config.before :each do
+    Sunspot.remove_all!
+  end
+end
