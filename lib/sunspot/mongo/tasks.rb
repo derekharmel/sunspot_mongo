e98396cc7c285d72e4a4d272a13efa744af56b55
Daniel Hoelbling-Inzko <tigraine@tigraine.at>
2015-06-11 13:02:30 +0200

    Fixed rake reindex bug when models are namespaced

--- lib/sunspot/mongo/tasks.rb
+++ lib/sunspot/mongo/tasks.rb
@@ -6,7 +6,11 @@ namespace :sunspot do
                          args[:models].split('+').map(&:constantize)
                        else
                          all_files = Dir.glob(Rails.root.join('app', 'models', '**', '*.rb'))
-                         all_models = all_files.map { |path| File.basename(path, '.rb').camelize.constantize }
+                         all_models = all_files.map do |path|
+                           dir = Pathname.new(path).relative_path_from(Rails.root.join('app/models'))
+                           dir = dir.to_s.gsub(/#{File.extname(dir)}$/, '')
+                           dir.camelize.constantize
+                         end
                          all_models.select { |m| m.include?(Sunspot::Mongo) && m.searchable? }
                        end
 
